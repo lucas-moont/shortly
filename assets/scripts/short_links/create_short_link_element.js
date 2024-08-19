74a0795522cba26f09907b59e925abd4bcbd130a
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-08-19 20:24:50 -0300

    --feat Adding html structure.

--- assets/scripts/short_links/create_short_link_element.js
+++ assets/scripts/short_links/create_short_link_element.js
@@ -7,20 +7,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import { GetUrlErrorHandling } from "./errors/get-url-global-error-handling";
 import { getShortenedLink } from "./get_shortened_link";
-const shortLinkForm = document.querySelector('.shortLinkGenerator');
-const shortLinkInput = document.querySelector('.shortLinkInput');
+const shortLinkForm = document.querySelector(".shortLinkGenerator");
+const shortLinkInput = document.querySelector(".shortLinkInput");
+const errorText = document.querySelector(".errorText");
 if (shortLinkForm && shortLinkInput) {
-    shortLinkForm.addEventListener('submit', (e) => __awaiter(void 0, void 0, void 0, function* () {
+    shortLinkForm.addEventListener("submit", (e) => __awaiter(void 0, void 0, void 0, function* () {
         e.preventDefault();
-        shortLinkForm.querySelector('.');
+        yield createShortLinkHTMLStructure();
     }));
 }
-function createShortLinkElement(e, url) {
+function createShortLinkHTMLStructure(url, shortenedLinksUl, errorText) {
     return __awaiter(this, void 0, void 0, function* () {
-        e.preventDefault();
         const shortenedUrlObject = yield getShortenedLink(url);
-        if (shortenedUrlObject.status === 'error') {
+        if (shortenedUrlObject.status === "error") {
+            if (errorText) {
+                throw new GetUrlErrorHandling(errorText);
+            }
+            throw new Error("Erro");
         }
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const button = document.createElement("button");
+        const completeLinkSpan = document.createElement("span");
+        const shortenedLinksInteraction = document.createElement("span");
+        li.classList.add("shortenedLinkLi");
+        a.classList.add("shortenedLinkAnchor");
+        button.classList.add("copyShortenedLink", "cyanBtn", "lessRoundedBtn");
+        completeLinkSpan.classList.add("completeLink");
+        shortenedLinksInteraction.classList.add("shortenedLinksInteraction");
+        a.href = shortenedUrlObject.shortUrl;
+        a.innerText = shortenedUrlObject.shortUrl;
+        button.innerText = "Copy";
+        button.addEventListener("click", () => {
+            navigator.clipboard.writeText("Copiado");
+        });
+        shortenedLinksInteraction.append(a, button);
+        completeLinkSpan.innerText = url;
+        li.append(completeLinkSpan, shortenedLinksInteraction);
+        shortenedLinksUl.appendChild(li);
     });
 }
--- typescript/short_links/create_short_link_element.ts
+++ typescript/short_links/create_short_link_element.ts
@@ -1,19 +1,56 @@
+import { GetUrlErrorHandling } from "./errors/get-url-global-error-handling"
 import { getShortenedLink } from "./get_shortened_link"
 
-const shortLinkForm = document.querySelector('.shortLinkGenerator')
-const shortLinkInput = document.querySelector('.shortLinkInput')
+const shortLinkForm = document.querySelector(".shortLinkGenerator")
+const shortLinkInput: HTMLInputElement | null = document.querySelector(".shortLinkInput")
+const errorText: HTMLParagraphElement | null =
+  document.querySelector(".errorText")
 
-if(shortLinkForm && shortLinkInput){
-  shortLinkForm.addEventListener('submit', async (e) => {
+if (shortLinkForm && shortLinkInput) {
+  shortLinkForm.addEventListener("submit", async (e) => {
     e.preventDefault()
-    shortLinkForm.querySelector('.')
+    await createShortLinkHTMLStructure()
   })
 }
 
-async function createShortLinkElement(e: Event, url: string) {
-  e.preventDefault()
+async function createShortLinkHTMLStructure(
+  url: string,
+  shortenedLinksUl: HTMLUListElement,
+  errorText: HTMLParagraphElement
+) {
   const shortenedUrlObject = await getShortenedLink(url)
-  if(shortenedUrlObject.status === 'error'){
-    
+  if (shortenedUrlObject.status === "error") {
+    if (errorText) {
+      throw new GetUrlErrorHandling(errorText)
+    }
+
+    throw new Error("Erro")
   }
+
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  const button = document.createElement("button")
+  const completeLinkSpan = document.createElement("span")
+  const shortenedLinksInteraction = document.createElement("span")
+
+  li.classList.add("shortenedLinkLi")
+  a.classList.add("shortenedLinkAnchor")
+  button.classList.add("copyShortenedLink", "cyanBtn", "lessRoundedBtn")
+  completeLinkSpan.classList.add("completeLink")
+  shortenedLinksInteraction.classList.add("shortenedLinksInteraction")
+
+  a.href = shortenedUrlObject.shortUrl
+  a.innerText = shortenedUrlObject.shortUrl
+  button.innerText = "Copy"
+
+  button.addEventListener("click", () => {
+    navigator.clipboard.writeText("Copiado")
+  })
+
+  shortenedLinksInteraction.append(a, button)
+  completeLinkSpan.innerText = url
+
+  li.append(completeLinkSpan, shortenedLinksInteraction)
+
+  shortenedLinksUl.appendChild(li)
 }
